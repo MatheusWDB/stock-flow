436ea3f6d0aa502258e69bf0f91b76e8d55a6026
MatheusWDB <matheuswdb1703@gmail.com>
2025-04-19 14:08:40 -0300

    Menu de navegação

---
+++ frontend/lib/widgets/main_menu.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/models/product.dart';
+import 'package:frontend/models/stock_movement.dart';
+import 'package:frontend/screens/dashboard_screen.dart';
+import 'package:frontend/screens/movements_screen.dart';
+import 'package:frontend/screens/product_list_screen.dart';
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({
+    required this.currentRoute,
+    required this.products,
+    required this.movements,
+    super.key,
+  });
+
+  final String currentRoute;
+  final List<Product> products;
+  final List<StockMovement> movements;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DashboardScreen(),
+                  ),
+                );
+              },
+              child: Text(
+                'Início',
+                style: style('/dashboard'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductListScreen(
+                      products: products,
+                      movements: movements,
+                    ),
+                  ),
+                );
+              },
+              child: Text(
+                'Produtos',
+                style: style('/products'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MovementsScreen(
+                      movements: movements,
+                      products: products,
+                    ),
+                  ),
+                );
+              },
+              child: Text(
+                'Movimentações',
+                style: style('/movements'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Relatórios',
+                style: style('/reports'),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        )
+      ],
+    );
+  }
+
+  TextStyle style(String route) {
+    return TextStyle(
+      color: currentRoute == route
+          ? const Color(0xFF551A8B)
+          : const Color(0xFF0000EE),
+      fontWeight: currentRoute == route ? FontWeight.bold : FontWeight.normal,
+    );
+  }
+}
